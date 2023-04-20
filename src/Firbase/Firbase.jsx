b45a356598af3f931bf581c9aeb9bf29d9999d7a
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-04-20 17:58:46 +0600

    setup router,firbase,tailwind,daisyUI

---
+++ src/Firbase/Firbase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpHHHg518Rg-9pc8rhChdZuscVw8DzHz8",
+  authDomain: "auth-firbase-context-tai-6f642.firebaseapp.com",
+  projectId: "auth-firbase-context-tai-6f642",
+  storageBucket: "auth-firbase-context-tai-6f642.appspot.com",
+  messagingSenderId: "715222371895",
+  appId: "1:715222371895:web:0d01ede53e9562ca7679b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
